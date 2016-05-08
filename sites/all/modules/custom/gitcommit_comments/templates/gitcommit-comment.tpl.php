b3de1239959904d9493227c8759ad9ace3c28c41
Tiago Bember <bember@chuva-inc.com>
2016-05-07 21:43:12 -0300

    Adding Drupal, contrib modules, bootstrap theme and features.

---
+++ sites/all/modules/custom/gitcommit_comments/templates/gitcommit-comment.tpl.php
@@ -0,0 +1,4 @@
+<div class="gitcommit-comment">
+  <strong><?php print t('Author'); ?>: </strong><span><?php print $author_name; ?> &lt;<?php print $author_mail; ?>&gt;</span>
+  <p><?php print $message; ?></p>
+</div>
